bc3b58c544405a02270675511de72dcab684adc4
Daniel Smith <malorian@me.com>
2016-05-26 15:53:37 -0400

    Added first Euler problem

---
+++ ProjectEuler/Problem1/np_python.py
@@ -0,0 +1,13 @@
+import time
+import numpy as np
+
+max_value = int(1e6)
+
+arr = np.arange(max_value)
+t = time.time()
+result = np.sum(arr[((arr % 3) == 0) | ((arr % 5) == 0)])
+print("The numpy result is %d" % result)
+print("total time taken %3.3f" % (time.time() - t))
+
+
+
