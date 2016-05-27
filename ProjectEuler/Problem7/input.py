ac317285fb2e077796fa2affeb3ccee4a9cc61d3
schiebermc <matthewschieber@gmail.com>
2016-05-27 15:46:11 -0400

    I added a solution to Problem 7

---
+++ ProjectEuler/Problem7/input.py
@@ -0,0 +1,24 @@
+import time
+import math
+
+start = time.time()
+
+print ("Problem 7: Using Trial Division to Check for Primality")
+
+count = 2
+test = 3
+
+while (count < 10001 ):
+    flag = 1
+    test += 2
+    for j in range(2, int(math.sqrt(test)) + 1):
+        if(test % j==0):
+            flag = 0
+            break
+    if(flag==1):
+        count +=1
+
+
+print("The 10001st  prime number is %d" % test)
+print("Total time took %6.3f" % (time.time() - start))
+
