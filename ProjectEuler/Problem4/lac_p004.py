99b90749b9ed0f3ec989f9459b4ad95d769d49e9
schiebermc <matthewschieber@gmail.com>
2016-06-02 12:12:17 -0400

    Revamped P. 4 and solved P. 19

--- ProjectEuler/Problem4/lac_p004.py
+++ ProjectEuler/Problem4/lac_p004.py
@@ -15,9 +15,21 @@ for m in range(x.size):
          pal=False
          break
       else:
-         pal = True
+        pal=True 
     if pal:
      if y[m,n] > element:
        element = y[m,n]
 print element
 print ("Time to run the program is: %f" % (time.time()-t))
+
+"""Notes: 
+- np.outer is good use of numpy library here.
+- Checks for Palidromes quckly.
+- Only checks half of the products (avoids duplicates).
+- Saves only the largest palidrome (element).
+- Not sure why this performs slower than Euler4.py.  Perhaps navigating
+  the matrix y takes more time, since the products of Euler4.py were saved
+  and scanned linearly.
+- Greatest speedup would be to replace for loops with while loops and scan
+  backwards, stopping after the greatest palidrome product is found.
+""" 
